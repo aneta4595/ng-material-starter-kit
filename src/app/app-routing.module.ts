5d8ed3bc696113b07d2bbc4ebe1bb23f9a3aa7a0
Aneta Walicka <aneta4595@wp.pl>
2022-11-11 12:28:41 +0100

    filtered list - on subject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FilteredListRoutingComponent } from './components/filtered-list-routing/filtered-list-routing.component';
+import { FilteredListSubjectComponent } from './components/filtered-list-subject/filtered-list-subject.component';
 import { FilteredListRoutingComponentModule } from './components/filtered-list-routing/filtered-list-routing.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
+import { FilteredListSubjectComponentModule } from './components/filtered-list-subject/filtered-list-subject.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
 
 @NgModule({
 
-  imports: [RouterModule.forRoot([{ path: 'products/:category', component: FilteredListRoutingComponent }]), FilteredListRoutingComponentModule, CategoriesServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products/:category', component: FilteredListRoutingComponent }, { path: 'products', component: FilteredListSubjectComponent }]), FilteredListRoutingComponentModule, CategoriesServiceModule, FilteredListSubjectComponentModule, ProductsServiceModule],
   exports: [RouterModule]
 })
 
